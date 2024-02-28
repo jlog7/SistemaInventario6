b41f022abc08c8dc711a8e46e88fe6da026df3ca
Jose Ortega <joseluisortegagila@gmail.com>
2024-02-28 12:06:26 +0100

    Se añade tabla bodega

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El Nombre es obligatorio")]
+        [MaxLength(60, ErrorMessage = "El Nombre no puede ser mayor de 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La descripcion es obligatoria")]
+        [MaxLength(100, ErrorMessage = "La descripcion no puede ser mayor de 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El estado es obligatorio")]
+        public bool Estado { get; set; }
+
+    }
+}
