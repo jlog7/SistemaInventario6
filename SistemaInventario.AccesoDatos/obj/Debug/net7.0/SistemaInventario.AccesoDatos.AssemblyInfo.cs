96f10b5af61b29a32866438963256e98266e6f14
Jose Ortega <joseluisortegagila@gmail.com>
2024-03-07 14:04:13 +0100

    CRUD de Bodegas

--- SistemaInventario.AccesoDatos/obj/Debug/net7.0/SistemaInventario.AccesoDatos.AssemblyInfo.cs
+++ SistemaInventario.AccesoDatos/obj/Debug/net7.0/SistemaInventario.AccesoDatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventario.AccesoDatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97beef91c49afdcd2de190eb209a2b48e444d592")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e1733e597b931ae015aacd181824444b7c1bef")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventario.AccesoDatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventario.AccesoDatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
